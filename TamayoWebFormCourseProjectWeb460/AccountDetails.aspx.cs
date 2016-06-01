be17766c05d6f393c6c9b3d8ce07fd432f596f99
CarlosK16 <CarlosK16@MONEYMAKER>
2016-05-31 22:24:12 -0400

    Got dataset to work properly.

--- TamayoWebFormCourseProjectWeb460/AccountDetails.aspx.cs
+++ TamayoWebFormCourseProjectWeb460/AccountDetails.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
 
 namespace TamayoWebFormCourseProjectWeb460
 {
@@ -13,5 +15,44 @@ namespace TamayoWebFormCourseProjectWeb460
         {
 
         }
+
+        //protected void btnSearch_Click(object sender, EventArgs e)
+        //{
+        //    DataSet dsSearch;
+
+        //    //string tempPath = Server.MapPath("Accounts.mdb");
+        //    string tempPath = Server.MapPath("~/App_Code/web460crsProject.accdb");
+            
+        //    //ClsDataLayer dataLayerObj = new ClsDataLayer(tempPath);
+
+        //    try
+        //    {
+        //        dsSearch = dataLayerObj.FindCustomer(txtUserSearch.Text);
+
+        //        if (dsSearch.UserAccount.Rows.Count > 0)
+        //        {
+        //            txtCity.Text = dsSearch.UserAccount[0].city;
+        //            txtState.Text = dsSearch.UserAccount[0].state;
+        //            txtFavLanguage.Text = dsSearch.UserAccount[0].favLanguage;
+        //            txtLeastFavLanguage.Text = dsSearch.UserAccount[0].leastFavLanguage;
+        //            txtDate.Text = dsSearch.UserAccount[0].dateOfLastProgramCompleted.ToString();
+
+        //            //customerID.Text = dsFindLastName.tblCustomers[0].CustomerID.ToString();
+
+        //            Master.UserFeedBack.Text = "Record Found";
+        //        }
+        //        else
+        //        {
+        //            Master.UserFeedBack.Text = "No records were found!";
+
+        //        }
+        //    }
+        //    catch (Exception error)
+        //    {
+        //        string message = "Something went wrong - ";
+        //        Master.UserFeedBack.Text = message + error.Message;
+
+        //    }
+        //}
     }
 }
---
+++ TamayoWebFormCourseProjectWeb460/AppCode/ClsDataLayer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace TamayoWebFormCourseProjectWeb460.AppCode
+{
+    public class ClsDataLayer
+    {
+        public ClsDataLayer()
+        {
+
+        }
+        OleDbConnection dbConnection;
+        public ClsDataLayer(string Path)
+        {
+            dbConnection = new OleDbConnection("PROVIDER=Microsoft.ACE.OLEDB.12.0;Data Source=" + Path);
+        }
+
+        public DataSet FindCustomer(string userName)
+        {
+            //Creates a connection and query the Customer table for the last name enetered 
+            string sqlStmt = "select * from UserAccount where userName like '" + userName + "'";
+            OleDbDataAdapter sqlDataAdapter = new OleDbDataAdapter(sqlStmt, dbConnection);
+
+            //creates dataset object
+            ProgramaholicsDataSet myStoreDataSet = new ProgramaholicsDataSet();
+
+            sqlDataAdapter.Fill(myStoreDataSet.UserAccount);
+
+            return myStoreDataSet;//returns customer's information
+        }
+
+        //Method that Updates Customer information in the database
+        public void UPdateAccount(string city, string state, string favProgLanguage, string leastFavProgLanguage, string dateLastProgCompleted)
+        {
+
+
+            //SQL update statement
+            string sqlStmt =
+                @"UPDATE UserAccount 
+            SET 
+            city = @city,
+            state = @state,
+            favLanguage = @favProgLanguage,
+            leastFavLanguage = @leastFavProgLanguage,
+            dateOfLastProgramCompleted = @dateLastProgCompleted,
+            WHERE UserAccount.userID = @userID";
+
+
+
+            //Object created to establish a connection and executes update statement
+            OleDbCommand dbCommand = new OleDbCommand(sqlStmt, dbConnection);
+
+
+            //Adds updated information to the database
+            //OleDbParameter param = new OleDbParameter("@first", firstName); //This line of code was incorrect and commented out.
+
+            dbCommand.Parameters.Add(new OleDbParameter("@city", city));
+            dbCommand.Parameters.Add(new OleDbParameter("@state", state));
+            dbCommand.Parameters.Add(new OleDbParameter("@favLanguage", favProgLanguage));
+            dbCommand.Parameters.Add(new OleDbParameter("@leastFavLanguage", leastFavProgLanguage));
+            dbCommand.Parameters.Add(new OleDbParameter("@dateOfLastProgramCompleted", dateLastProgCompleted));
+            //dbCommand.Parameters.Add(new OleDbParameter("@userId", userID));
+
+            try
+            {
+                //Opens database connection
+                dbConnection.Open();
+                //Executes
+                dbCommand.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                //Ensures the connection always closes
+                dbConnection.Close();
+            }
+        }
+
+        public DataSet GetPastApplications()
+        {
+            //Used to fill in the DataSet and update the data source
+            OleDbDataAdapter sqlDataAdapter = new OleDbDataAdapter("SELECT * FROM PastApplications;", dbConnection);
+
+            //Fills in the dataset
+            ProgramaholicsDataSet myStoreDataSet = new ProgramaholicsDataSet();
+            sqlDataAdapter.Fill(myStoreDataSet.PastApplications);
+
+            //Returns the infomation stored in the table Customers
+            return myStoreDataSet;
+        }
+    }
+}
